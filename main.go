d2cfec8de9ab0a11bf63d74b1f38e5b59035dce4
nade-harlow <franklynomonade@gmail.com>
2021-07-27 20:43:57 +0100

    Variable assignment

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	var name string = "Franklyn"
 	var stack string = "GoLang"
 	var squad string = "008"
 	var year int = 2021
 
-	fmt.Println("My name is", name," ,I am a ", stack, "stack developer a member of squad", squad,"year of", year)
+	fmt.Println("My name is ", name, " ,I am a ", stack, " stack developer a member of squad ", squad, " year of ", year)
 
 }
